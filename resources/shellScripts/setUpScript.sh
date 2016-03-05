07649b5c58815dc3831e15753c7f94d2554597e8
Minh Nguyen <nguyenhminhp@gmail.com>
2016-03-05 11:32:32 -0800

    testing ssh script

--- resources/shellScripts/setUpScript.sh
+++ resources/shellScripts/setUpScript.sh
@@ -10,4 +10,4 @@ curl -O 'https://nodejs.org/dist/v4.3.1/node-v4.3.1-linux-x64.tar.xz' && tar -xf
 echo "export PATH=/home/ubuntu/node-v4.3.1-linux-x64/bin:$PATH" >> ~/.bashrc;
 source ~/.bashrc;
 cd smallscale && npm install;
-screen -dm bash ./shellScripts/runNode.sh
+# screen -dm bash ./shellScripts/runNode.sh
