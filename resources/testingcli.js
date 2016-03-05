48ad56f45c5bdb3c9456757e200334cbc19473ed
Minh Nguyen <nguyenhminhp@gmail.com>
2016-03-05 10:03:11 -0800

    testing ssh script

--- resources/testingcli.js
+++ resources/testingcli.js
@@ -72,41 +72,41 @@
 // child process
 // var int = 755;
 var childProcess = require('child_process').spawn;
-var scp = childProcess('scp', 
-    ['-r', 
-      '-o StrictHostKeyChecking=no',
-      '-i',
-      '/home/ubuntu/scaleapp1.pem',
-      '/home/ubuntu/smallscale/resources/shellScripts',
-      'ubuntu@ec2-52-24-109-81.us-west-2.compute.amazonaws.com:/home/ubuntu']
-  )
-scp.stdout.on('data', (data) => {
-  console.log(`stdout: ${data}`);
-});
-
-scp.stderr.on('data', (data) => {
-  console.log(`stderr: ${data}`);
-});
-
-scp.on('close', (code) => {
-  console.log(`child process exited with code ${code}`);
-});
-// // spawn the slave using slaveId as the key
-// var ssh = childProcess('ssh', [
-//     '-i',
-//     'C:/Users/minh_/Desktop/Scalability/resources/scaleapp1.pem',
-//     'ubuntu@52.36.238.114',
-//     'cd shellScripts && chmod +x ./runNode.sh'
-//   ])
-
-// ssh.stdout.on('data', (data) => {
+// var scp = childProcess('scp', 
+//     ['-r', 
+//       '-o StrictHostKeyChecking=no',
+//       '-i',
+//       '/home/ubuntu/scaleapp1.pem',
+//       '/home/ubuntu/smallscale/resources/shellScripts',
+//       'ubuntu@ec2-52-24-109-81.us-west-2.compute.amazonaws.com:/home/ubuntu']
+//   )
+// scp.stdout.on('data', (data) => {
 //   console.log(`stdout: ${data}`);
 // });
 
-// ssh.stderr.on('data', (data) => {
+// scp.stderr.on('data', (data) => {
 //   console.log(`stderr: ${data}`);
 // });
 
-// ssh.on('close', (code) => {
+// scp.on('close', (code) => {
 //   console.log(`child process exited with code ${code}`);
+    // spawn the slave using slaveId as the key
+  var ssh = childProcess('ssh', [
+      '-i',
+      'C:/Users/minh_/Desktop/Scalability/resources/scaleapp1.pem',
+      'ubuntu@52.36.238.114',
+      'chmod +x ./shellScripts/runNode.sh'
+    ])
+
+  ssh.stdout.on('data', (data) => {
+    console.log(`stdout: ${data}`);
+  });
+
+  ssh.stderr.on('data', (data) => {
+    console.log(`stderr: ${data}`);
+  });
+
+  ssh.on('close', (code) => {
+    console.log(`child process exited with code ${code}`);
+  });
 // });
